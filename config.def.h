a0948cb1e96149e61471a6ed70f15614e7fccbaf
jianggl <jianggl6827@gmail.com>
2022-10-30 21:18:31 +0800

    add patch scratchpad
    使用快捷键打开一个浮动窗口，默认是st

--- config.def.h
+++ config.def.h
@@ -1,25 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 5;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "jetBrainsMono nerd font mono:style=Regular:pixelsize=16:antialias=true:autohint=true", "wenquanyi micro hei:style=Regular:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "jetBrainsMono nerd font mono:style=Regular:pixelsize=16:antialias=true:autohint=true";
-static const char col_gray1[]       = "#1F2D3A";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#2AA198";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int borderpx    = 2;        /* border pixel of windows */
+static const unsigned int snap        = 32;       /* snap pixel */
+static const unsigned int gappih      = 5;        /* horiz inner gap between windows */
+static const unsigned int gappiv      = 5;        /* vert inner gap between windows */
+static const unsigned int gappoh      = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps            = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar              = 1;        /* 0 means no bar */
+static const int topbar               = 1;        /* 0 means bottom bar */
+static const char *fonts[]            = { "jetBrainsMono nerd font mono:style=Regular:pixelsize=16:antialias=true:autohint=true", "wenquanyi micro hei:style=Regular:size=10:antialias=true:autohint=true" };
+static const char dmenufont[]         = "jetBrainsMono nerd font mono:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static const char col_gray1[]         = "#1F2D3A";
+static const char col_gray2[]         = "#444444";
+static const char col_gray3[]         = "#bbbbbb";
+static const char col_gray4[]         = "#eeeeee";
+static const char col_cyan[]          = "#2AA198";
+static const unsigned int baralpha    = 0xc0;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
+static const char *colors[][3]        = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
@@ -82,8 +82,10 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]      = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]       = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -136,6 +138,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_o,      togglescratch,  {.v = scratchpadcmd } },
 	{ Mod1Mask,                     XK_F3,     spawn,          SHCMD("light -U 2") },
 	{ Mod1Mask,                     XK_F4,     spawn,          SHCMD("light -A 2") },
 	{ Mod1Mask,                     XK_F5,     spawn,          SHCMD("amixer sset Master 0%") },
